3a3bb43eaf02afa1a66aebe309749b388e50be7c
GorokhovaPolina <128795394+GorokhovaPolina@users.noreply.github.com>
2025-05-21 15:09:17 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/studybuddysystem/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
